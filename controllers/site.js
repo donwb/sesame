e03a6ceb18759a0a6b367b374e2bcd5e7f9bfc54
Don Browning <don.browning@gmail.com>
2012-07-21 01:03:37 -0400

    responding to texts w/a mls number in the body

--- controllers/site.js
+++ controllers/site.js
@@ -6,20 +6,23 @@ exports.index = function(req, res){
 };
 
 exports.post = function(req, res){
-	var from = ('req body: ' + req.body.From);
-	var message = 'echoing From: ' + from + ' to: ' + req.body.To + ' body: ' + req.body.Body;
-	var response = '<?xml version="1.0" encoding="UTF-8"?><Response><Sms>' + message + '</Sms></Response>'
+	var body = req.body.Body;
+	//var message = 'echoing From: ' + from + ' to: ' + req.body.To + ' body: ' + req.body.Body;
+	//var response = '<?xml version="1.0" encoding="UTF-8"?><Response><Sms>' + message + '</Sms></Response>'
 
-  res.send(response);
-};
-
-exports.test = function(req, res){
-	var from = ('req body: ' + req.body.From);
-	var message = 'echoing From: ' + from + ' to: ' + req.body.To + ' body: ' + req.body.Body;
-	var response = '<?xml version="1.0" encoding="UTF-8"?><Response><Sms>' + message + '</Sms></Response>'
-
-	HouseProvider.getAll(function(err, homes){
-		console.log(homes);
+	console.log(body);
+	HouseProvider.getHouse(body, function(err, house){
+		if(house === null){
+			var message = 'cant find that mls number';
+			var response = '<?xml version="1.0" encoding="UTF-8"?><Response><Sms>' + message + '</Sms></Response>'
+			res.send(response);			
+		}else{
+			HouseProvider.stamp(house, function(err){
+				console.log(house.key);
+				var message = 'They key is: ' + house.key;
+				var response = '<?xml version="1.0" encoding="UTF-8"?><Response><Sms>' + message + '</Sms></Response>'
+				res.send(response);
+			});
+		}
 	});
-  res.send(response);
-}
+};
--- models/HouseProvider.js
+++ models/HouseProvider.js
@@ -5,9 +5,16 @@ var mongoose = require('mongoose');
 
 var Schema = mongoose.Schema, ObjectID = Schema.ObjectId;
 
+var Requests = new Schema({
+	phone	: {type: String, required: true},
+	userid	: {type: String, required: true},
+	date 	: {type: String, required: true}
+});
+
 var House = new Schema({
     MLS	            : {type: String, required: true},
-    key			    : {type: String, required: true}
+    key			    : {type: String, required: true},
+    requests		: [Requests]
 });
 
 mongoose.connect('mongodb://' + db.user + ':' + db.pass + '@' + db.host + ':' + db.port + '/' + db.name);
@@ -20,7 +27,22 @@ HouseProvider = function(){};
 HouseProvider.prototype.getAll = function(callback) {
 	House.find({}, function(err, houses){
 		callback(null, houses);
-	})
+	});
 };
+
+HouseProvider.prototype.getHouse = function(mls, callback){
+	var mlstr = mls.toString();
+	console.log(mlstr);
+	House.findOne({MLS:mlstr}, function(err, house){
+		callback(null, house);
+	});
+};
+
+HouseProvider.prototype.stamp = function(house, callback){
+	house.requests.push({phone: '4444', userid: '111', date: 'a date'});
+	
+	house.save();
+	callback(null);
+}
 
 exports.HouseProvider = HouseProvider;
